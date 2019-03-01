20514571e81ab9de88d615916869152a436aa1a0
EricChan <31045561+ericflyfly@users.noreply.github.com>
2019-03-01 00:40:03 -0500

    Created checkword.py to cheat in WordsStory
    -function to find all possible word combinations from a list of words (no duplicates and sorted)
    -user can choose the final result as existing word or any word

---
+++ checkWord.py
@@ -0,0 +1,52 @@
+import enchant
+import os
+
+#init dictionary
+d = enchant.Dict("en_US")
+
+#function that start find possible word combinations recursively
+def findCombo(letters, maxChar, existWord):
+	if (not existWord):	
+		return findComboHelper(letters.upper(), "", maxChar)
+	else:
+		return findComboHelperExist(letters.upper(), "", maxChar)
+
+#function to find all possible word combninations recursively
+def findComboHelper(letters, str, maxChar):
+    if (not letters) or (maxChar <= 0):
+        return {str}
+    result = set()
+    for i in range(len(letters)):
+        result = result.union(findComboHelper(letters[0: i] + letters[i+1: len(letters)], str+letters[i], maxChar - 1))
+    return sorted(result)
+
+#function to find word combninations that exist only
+def findComboHelperExist(letters, str, maxChar):
+    if (not letters) or (maxChar <= 0):
+    	global d
+    	if (d.check(str)):
+        	return {str}
+    	else:
+        	return set()
+    result = set()
+    for i in range(len(letters)):
+        result = result.union(findComboHelperExist(letters[0: i] + letters[i+1: len(letters)], str+letters[i], maxChar - 1))
+    return sorted(result)
+    
+#ask for input
+letters = input ("What are your letters? ")
+maxLength = input ("What is the max length of the word? ")
+existWord = input ("Do you want words that exist only?(y/n) ")
+
+#change mode based on input
+if "y" in str(existWord).lower():
+	existWord = True
+else:
+	existWord = False
+
+#print result
+result = findCombo(str(letters), int(maxLength), existWord)
+print ("Possible combinations: ", len(result))
+for combo in result:
+    print (combo)
+    os.system ("say " + combo)
