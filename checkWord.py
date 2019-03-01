4dd6e318e20b650d39c43a9e5fbde0347547ce8e
honwingericchan@gmail.com <31045561+ericflyfly@users.noreply.github.com>
2019-03-01 18:16:37 -0500

    added function to allow user specific letters in the output strings

--- checkWord.py
+++ checkWord.py
@@ -22,36 +22,32 @@ def findComboHelper(letters, str, maxChar):
         result = result.union(findComboHelper(letters[0: i] + letters[i+1: len(letters)], str+letters[i], maxChar - 1))
     return sorted(result)
 
-#function to find word combninations that exist only
-"""def findComboHelperExist(letters, str, maxChar):
-    if (not letters) or (maxChar <= 0):
-    	global d
-    	if (d.check(str)):
-        	return {str}
-    	else:
-        	return set()
-    result = set()
-    for i in range(len(letters)):
-        result = result.union(findComboHelperExist(letters[0: i] + letters[i+1: len(letters)], str+letters[i], maxChar - 1))
-    return sorted(result)
-    """
-
 #ask for input
-letters = input ("What are your letters? ")
-maxLength = input ("What is the max length of the word? ")
+letters = input ("What are your possible letters? ")
+maxLength = input ("What is the length of the word? ")
 existWord = input ("Do you want words that exist only?(y/n) ")
-
+containLetters = str(input ("sepecific Letters in the word? "))
 #change mode based on input
 if "y" in str(existWord).lower():
 	existWord = True
-else:
+elif "n" in str(existWord).lower():
 	existWord = False
+else:
+    raise ValueError("Wrong input")
 
 #print result
 result = findCombo(str(letters), int(maxLength))
+
 print ("Possible combinations: ", len(result))
 for combo in result:
-    #display result
-    print (combo)
-    #read out each result in terminal
-    os.system ("say " + combo)
+    check = True
+    if containLetters:
+        for char in containLetters:
+            if char not in combo:
+                check = False
+                break
+    if check:
+        #display result
+        print (combo)
+        #read out each result in terminal
+        os.system ("say " + combo)
